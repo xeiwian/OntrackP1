a9b61934e13b177003cc83559e8f50f821b7875c
xeiwian <45346081+xeiwian@users.noreply.github.com>
2019-06-21 17:49:21 +0800

    get the html page to work on localhost/3000 for real without affecting other routes

--- script.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-}).listen(port); //the server object listens on port 
