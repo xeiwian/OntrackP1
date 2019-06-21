3218b3bc3871372974e8f8e68f18fed3f8dbddae
xeiwian <45346081+xeiwian@users.noreply.github.com>
2019-06-21 14:40:19 +0800

    successfully make api and db up and running

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require ('express');
-const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "us-west-2",
+    endpoint: "http://localhost:8000"
+});
+
+const dynamodb = new AWS.DynamoDB();
 
 const app = express();
 
-const port = 8000;
+const port = 3000;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -14,8 +21,72 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-require('./app/routes')(app, {});
+// this only purpose is to test the API 
+app.post('/testing', (req, res) => {
+    console.log(req.body)
+    res.send('hey')
+});
+
+// GET the table
+app.get('/coupon', (req, res) => {
+        var params = {
+            TableName: 'CouponDBLocal',
+            // ProjectionExpression: 'couponID, couponCode'
+        };    
+        dynamodb.scan(params, function (err, data) {
+              if (err) console.log(err, err.stack); // an error occurred
+              else {
+                    res.send(data);
+                    console.log(data);
+              }
+        });
+});
+
+// put items into table
+app.post('/coupon', (req, res) => {
+    const params = {
+        Item: {
+            'userID_couponID': { N: '112' },
+            'DateTime': { N: '14062019192' },
+            'userID': { N: '1' },
+            'couponID': { N: '12' },
+            'couponCode': { S: 'ABC123' },
+        },
+        TableName: 'CouponDBLocal',
+        ReturnConsumedCapacity: "TOTAL",
+    };
+    
+    dynamodb.putItem(params, (err, data) => {
+        if (err) {
+            console.error(err, err.stack);
+        } else {
+            res.send(data);
+            console.log(data);
+        }
+    })
+});
+
+// retrieve item from table by its Partition Key and Sort Key
+app.get('/coupon/112', (req, res) => {
+    const params = {
+        TableName: 'CouponDBLocal',
+        Key:{
+            'userID_couponID': { N: '112' },
+            'DateTime': { N: '14062019192' }
+        }
+    };
+    dynamodb.getItem(params, (err, data) => {
+        if (err) {
+            console.error(err, err.stack);
+        } else {
+            res.send(data);
+            console.log(data);
+        }
+    })
+});
+
+// require('./app/routes')(app, {});
 
 app.listen(port, () => {
-    console.log("We are live in port " + port);
+    console.log("We are live on port " + port);
 })
