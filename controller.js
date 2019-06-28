ff31b6a845affefa4d3ef748e3dd4adfdb33df88
xeiwian <45346081+xeiwian@users.noreply.github.com>
2019-06-28 11:09:24 +0800

    completed project 1 main purpose

--- controller.js
+++ controller.js
@@ -15,19 +15,6 @@ function webpage(req, res) {
     res.sendFile('/test.html', {root: __dirname })
 }
 
-// function for query database
-function query(params) {
-    return new Promise((resolve,reject) => {
-        dynamodb.query(params, (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        });
-    })
-}
-
 // function for getting user info from database
 function getUser(params) {
     return new Promise((resolve,reject) => {
@@ -69,22 +56,18 @@ function get(params_table) {
     })
 }
 
-async function getCouponTable(req,res) {
-    // parameters for getting table info
-    var params_table = {
-        TableName: 'CouponLocalDB',
-    };  
-
+async function getCouponTable(req,res) { 
     try {
-        const getTable= await get(params_table);
+        const getTable= await get({
+            // parameters for getting table info
+            TableName: 'CouponLocalDB'
+        });
         console.log(getTable);
         res.send(getTable);
-
     } catch (err) {
         console.log('error from getting table',err, err.stack);
         throw err;
     }
-
 }
 
 // extract and insert user info from coupon button 
@@ -93,73 +76,48 @@ async function extractUserInfo(req, res) {
     var token = req.body.idToken;
     var couponID = req.body.couponid;
     var couponCODE = req.body.couponcode;
+    // to get a data without including its time
     var dateTIME = new Date().toISOString().substr(0,10);
 
     // verifying and decoding id token 
-    const decodedToken = await admin.auth().verifyIdToken(token);
+    var decodedToken = await admin.auth().verifyIdToken(token);
     var userID = decodedToken.user_id + ".";
 
     // concatenate user id and coupon id for storing it in database as Partition key
     var userID_couponID = userID.concat(couponID);
-
-    // parameters for inserting user info to database
-
-
-  
-
-    // parameters for querying user info in database
-    var params = {
-        ReturnConsumedCapacity: "TOTAL",
-        ExpressionAttributeValues: {
-            ":v1": {
-              S: userID_couponID
-            }
-           }, 
-            KeyConditionExpression: "user_id_coupon_id = :v1", 
-            // ProjectionExpression: "dateTime", 
-            TableName: 'CouponLocalDB'
-    };
-
-    // try {
-    //     const queryResult = await query(params);
-    //     console.log(queryResult);
-
-    // } catch (err) {
-    //     console.log('error from query',err, err.stack);
-    //     throw err;
-    // }
-
     
+
     try {
         const getUserInfo = await getUser({
+            // parameters for inserting user info to database
             Key: {
              "user_id_coupon_id": {
                S: userID_couponID
-              }, 
-             "dateTime": {
-               S: dateTIME
+              },
+             "coupon_id": {
+               S: couponID
               }
             }, 
             TableName: "CouponLocalDB"
         });
-        console.log(getUserInfo,userID_couponID ,dateTIME);
+        console.log(getUserInfo, userID_couponID, couponID);
         console.log('get info success');
 
         // {} if not found
         // { Item: {...} } if found
        
         if ( getUserInfo.Item === undefined ){
-            const insertResult = await insert( {
+            const insertUserInfo = await insert({
+                // parameters for inserting user info
                 Item: {
                     'user_id_coupon_id': { S: userID_couponID /* abc213 */ },
-                    'dateTime': { S: dateTIME /* 2019-06-27 */ },
                     'coupon_id': { S: couponID },
+                    'dateTime': { S: dateTIME /* 2019-06-27 */ },
                     'coupon_code': { S: couponCODE }
                 },
                 TableName: 'CouponLocalDB',
-               
             });
-            console.log(insertResult);
+            console.log(insertUserInfo);
             res.sendFile('/thank.html', {root: __dirname });
             console.log('item inserted success');
         } else {
@@ -170,67 +128,6 @@ async function extractUserInfo(req, res) {
         console.log('error from getting info',err, err.stack);
         throw err;
     }
-
-        // try {
-        //     const getUserInfo = await getUser(params);
-        //     const insertResult = await insert(params_insert);
-
-        //     console.log(getUserInfo);
-        //     console.log('get info success');
-
-        //     if ( getUserInfo.userID_couponID !== insertResult.userID_couponID ){
-        //         res.sendFile('/thank.html', {root: __dirname });
-        //         console.log('item inserted success');
-        //         // res.sendFile('/claimed.html', {root: __dirname });
-        //         // console.log('item exists');
-        //     } else {
-        //         // res.sendFile('/thank.html', {root: __dirname });
-        //         // console.log('item inserted success');
-        //         res.sendFile('/claimed.html', {root: __dirname });
-        //         console.log('item exists');
-        //     }
-        // } catch (err) {
-        //     console.log('error from getting info',err, err.stack);
-        //     throw err;
-        // }
-
-    // try {
-    //     const insertResult = await insert(params_insert);
-    //     console.log(insertResult);
-    //     console.log('item inserted success');
-    //     res.sendFile('/thank.html', {root: __dirname });
-    // } catch (err) {
-    //     console.log('error from getting info',err, err.stack);
-    //     throw err;
-    // }
-
-    // try {
-    //     const getUserInfo = await getUser(params);
-    //     console.log('get info success');
-
-    //     if (userID_couponID !== null){
-    //         res.sendFile('/claimed.html', {root: __dirname });
-    //         console.log('item exists');
-    //     }
-    // } catch (err) {
-    //     console.log('error from getting info',err, err.stack);
-    //     throw err;
-    // }
-
-    // try {
-    //     // if (userID_couponID == null) {
-    //     const insertResult = await insert(params_insert);
-    //     if (userID_couponID == null) {
-    //         console.log(insertResult);
-    //     } else {
-    //         console.log('item exists');
-    //     }
-    //     res.send('post ok')
-    // } catch (err) {
-    //     console.log('error from insert',err, err.stack);
-    //     res.send('post failure')
-    // }
-
 }
 
 module.exports = {
